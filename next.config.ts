94d0677fc4c26867c85bb9d433d9e418fa64b144
tkmaher <tkmaher@umich.edu>
2025-11-22 14:18:12 -0600

    hmm

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true, // Recommended for static exports to avoid issues with image optimization
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
